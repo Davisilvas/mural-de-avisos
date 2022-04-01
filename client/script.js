5298166c5bbd4376211f20994f3cee451343bda8
Davi Silva Santos <morg.daviss@gmail.com>
2022-03-31 22:53:55 -0300

    tentando

--- client/script.js
+++ client/script.js
@@ -1,1 +1,33 @@
-console.log("linkado")
+// let title = window.document.getElementById("title");
+// let description = window.document.getElementById("description");
+
+
+// // tMural = titulo do dMural
+// // dMural = descrição do mural
+
+// let tMural = window.document.getElementById("titleMural");
+// let dMural = window.document.getElementById("descriptionMural");
+
+// tMural = title.innerHTML
+
+
+function carregado(){
+    let titulo = localStorage.getItem("title");
+    let descricao = localStorage.getItem("description");
+
+    let title = document.getElementById("title");
+    let description = document.getElementById("description");
+
+    // title.innerHTML = titulo;
+    // description.innerHTML = descricao
+}
+
+function attTilte(element){
+    let valor = element.value;
+    console.log(valor);
+
+    let titulo = localStorage.getItem("titleMural");
+    titulo.innerHTML = valor;
+
+    localStorage.setItem("titleMural", valor);
+}
