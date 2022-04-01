b13acbf0f6608f08bc0c36c0540d99d1c5aadd6f
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-01 18:28:51 -0300

    terminei o back

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const PORT = 3000;
 const express = require('express');
-const bodyParser = require('body-parser');
-const posts = require('./model/posts')
+const path = require('path');
+const apiRoute = require('./routes/api')
+const app = express();
 
-const app = express()
+app.use(express.static(path.join(__dirname, "client")));
+app.use('/api', apiRoute);
 
 
-app.get("/all", (req, res) =>{
-    res.json(JSON.stringify(posts.getAll()));
-    // res.json(JSON.stringify(posts));
-})
-
-app.post("/new", bodyParser.json() ,(req, res) =>{
-
-    // let id = generateID();
-    let title = req.body.title;
-    let description = req.body.description;
-
-    posts.newPost(title, description);
-    // posts.push({id, title, description}); 
-    // // posts.push({"id":id, "title":title, "description":description});  
-    res.send("post adicionado com sucesso...")  
-})
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}...`)
 })
-//TUDO O QUE ESTÁ COMENTADO ACIMA PODE SER EXCLUÍDO, EU DEIXO AQUI COMENTADO SÓ PRA TER VISIVELMENTE A DIFERENÇA ENTRE UM JEITO E O OUTRO.
 
 // pra fazer isso que eu fiz acima que é colocar uma variável no meio de uma string a gente tem que colocar as crases e na hora que for chamar a variável a gente colocar ${nomeDaVariavel}.
