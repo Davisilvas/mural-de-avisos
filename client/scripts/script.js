9b5bb0f90ea65181edd7b25066157f4bb10f94c7
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-04 23:50:40 -0300

    finalizado

--- client/scripts/script.js
+++ client/scripts/script.js
@@ -33,5 +33,22 @@ function updatePost(){
 };
 
 function newPost(){
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    
+    let post = {title, description}
+
+    const options = {
+        method: "POST",
+        headers: new Headers({ 'content-type': 'application/json'}),
+        body: JSON.stringify(post)
+    }
+
+    fetch ("http://192.168.0.108:3000/api/new", options).then(res =>{
+        console.log(res);
+        updatePost();
+        document.getElementById("title").value = "";
+        document.getElementById("description").value="";
+    })
 
 };
